933761ce430f99d7c7eb35e8fa78dc5cd6437536
Nimtaa <nima.tavassoli2@gmail.com>
2018-12-25 19:47:34 +0330

    project updated

--- Taleghani cross-section/src/main.cpp
+++ Taleghani cross-section/src/main.cpp
@@ -1,119 +1,243 @@
 #include <mbed.h>
 
 // System Outputs
-DigitalOut N_S_Green(PTC1);
-DigitalOut N_S_Red(PTC1);
-DigitalOut N_S_Yellow(PTC1);
 
-DigitalOut W_E_Green(PTC1);
-DigitalOut W_E_Red(PTC1);
-DigitalOut W_E_Yellow(PTC1);
+DigitalOut N_S_Green(PTC7);
+DigitalOut N_S_Red(PTC0);
+// DigitalOut N_S_Yellow(PTC1);
 
-DigitalOut Turn_Green(PTC1);
-DigitalOut Turn_Red(PTC1);
-DigitalOut Turn_Yellow(PTC1);
+DigitalOut W_E_Green(PTC5);
+DigitalOut W_E_Red(PTC6);
+// DigitalOut W_E_Yellow(PTC1);
 
-DigitalOut BUS_Green(PTC1);
-DigitalOut BUS_Red(PTC1);
-DigitalOut BUS_Yellow(PTC1);
+DigitalOut Turn_Green(PTC3);
+DigitalOut Turn_Red(PTC4);
+// DigitalOut Turn_Yellow(PTC1);
+
+DigitalOut BUS_Green(PTC10);
+DigitalOut BUS_Red(PTC11);
+// DigitalOut BUS_Yellow(PTC1);
 
 
 // System Inputs
 InterruptIn button1(PTD1);
 
-DigitalIn push_button1(PTD1);
-DigitalIn push_button2(PTD2);
-DigitalIn push_button3(PTD3);
-
-
-
 //States
-#define N_S_STATE_Wait 0;
-#define N_S_STATE_Go 1;
-#define W_E_STATE_Wait 2 ;
-#define W_E_STATE_Go 3 ;
-#define Emergency_STATE  4;
-#define TURN_STATE_Go  5;
-#define TURN_STATE_Wait  6;
-#define BUS_STATE_Go 7;
-#define BUS_STATE_Wait 8;
+const int N_S_STATE_Go = 0;
+const int N_S_STATE_Wait = 1;
+const int W_E_STATE_Go = 2;
+const int W_E_STATE_Wait = 3;
+
 
 //Global Variables
-int curr_state;
-int transition[9]={
-      
-  };
-
-
-
-// Task Counters
-int task3_counter = 0;
-int task4_counter = 0;
-
-void scheduler();
-void check_flags() {
-    if(push_button1 == 0) {
-      but1_pressed = true;
-    }
-    if(push_button2 == 0) {
-      but2_pressed = true;
-    }
-    if(push_button3 == 0) {
-      but3_pressed = true;
-    }
-}
-
-
-
+bool Emergency_pushed = false;
+int curr_state = 0;
+void state_machine();
 
 void N_S_STATE_Go_task(){
-
-}
+  
+    if(Emergency_pushed){
+      N_S_Green = 0;
+      N_S_Red = 1;
+      W_E_Green =0;
+      W_E_Red = 1;
+      Turn_Red = 1;
+      Turn_Green = 0;
+      BUS_Green =0 ;
+      BUS_Red = 1;
+      wait(3);
+      Emergency_pushed = false;
+    }else{
+    N_S_Green = 1;
+    N_S_Red = 0;
+    W_E_Green =0 ;
+    W_E_Red = 1;
+    Turn_Green = 0;
+    Turn_Red = 1;
+    BUS_Green = 1;
+    BUS_Red = 0;
+    wait(2);
+    N_S_Green = 1;
+    N_S_Red = 0;
+    W_E_Green =0 ;
+    W_E_Red = 1;
+    Turn_Green = 1;
+    Turn_Red = 0;
+    BUS_Red = 1;
+    BUS_Green = 0;
+    wait(2);
+    }
+  }
 void N_S_STATE_Wait_task(){
-
+    N_S_Green = 1;
+    N_S_Red = 1;
+    W_E_Green =0;
+    W_E_Red = 1;
+    Turn_Red = 1;
+    Turn_Green = 0;
+    BUS_Green =0 ;
+    BUS_Red = 1;
+    wait(2);
+  
 }
 void W_E_STATE_Go_task(){
-
+  
+    if(Emergency_pushed){
+     N_S_Green = 0;
+    N_S_Red = 1;
+    W_E_Green =0;
+    W_E_Red = 1;
+    Turn_Red = 1;
+    Turn_Green = 0;
+    BUS_Green =0 ;
+    BUS_Red = 1;
+    wait(3);
+    Emergency_pushed = false;
+  }
+  else{
+    N_S_Green = 0;
+    N_S_Red = 1;
+    W_E_Green =1 ;
+    W_E_Red = 0;
+    BUS_Red = 1;
+    BUS_Green = 0;
+    Turn_Green = 0;
+    Turn_Red = 1;
+    wait(4);
+}
 }
 void W_E_STATE_Wait_task(){
-
+    N_S_Green = 0;
+    N_S_Red = 1;
+    W_E_Green =1 ;
+    W_E_Red = 1;
+    BUS_Red = 1;
+    BUS_Green = 0;
+    Turn_Green = 0;
+    Turn_Red = 1;
+    wait(2);
 }
-void TURN_STATE_Go_task(){
-
-}
-void TURN_STATE_Wait_task(){
-
-}
-void BUS_STATE_Go_task(){
-
-}
-void BUS_STATE_Wait_task(){
-
-}
+// void TURN_STATE_Go_task(){
+//   Turn_Green = 1;
+//   Turn_Red = 0;
+//   BUS_Green = 0;
+//   BUS_Red = 1;
+//   wait(4);
+// }
+// void TURN_STATE_Wait_task(){
+//   Turn_Green = 1;
+//   Turn_Red = 1;
+//   BUS_Green = 0;
+//   BUS_Red = 1;
+//   wait(2);
+// }
+// void BUS_STATE_Go_task(){
+//   BUS_Green = 1;
+//   BUS_Red = 0;
+//   Turn_Red  = 1;
+//   Turn_Green = 0;
+//   wait(4);
+// }
+// void BUS_STATE_Wait_task(){
+//   BUS_Green = 1;
+//   BUS_Red = 1;
+//   Turn_Red  = 1;
+//   Turn_Green = 0;
+//   wait(2);
+// }
 void Emergency_STATE_task(){
+  // Emergency_pushed = true;
   switch (curr_state) {
-    
-    case W_E_STATE_Wait:
-    
-    case N_S_STATE_Wait:
+     case N_S_STATE_Go :{
+        N_S_Green = 1;
+        N_S_Red = 1;
+        W_E_Green =0;
+        W_E_Red = 1;
+        Turn_Red = 1;
+        Turn_Green = 0;
+        BUS_Green =0 ;
+        BUS_Red = 1;
+        wait(2);
+        N_S_Green = 0;
+        N_S_Red = 1;
+        W_E_Green =0;
+        W_E_Red = 1;
+        Turn_Red = 1;
+        Turn_Green = 0;
+        BUS_Green =0 ;
+        BUS_Red = 1;
+        wait(3);
+        break;
+    }
+    case N_S_STATE_Wait:{
+      Emergency_pushed = true;
+      break;
+    }
+     case W_E_STATE_Go:{
+        N_S_Green = 0;
+        N_S_Red = 1;
+        W_E_Green =1 ;
+        W_E_Red = 1;
+        BUS_Red = 1;
+        BUS_Green = 0;
+        Turn_Green = 0;
+        Turn_Red = 1;
+        wait(2);
+        N_S_Green = 0;
+        N_S_Red = 1;
+        W_E_Green =0;
+        W_E_Red = 1;
+        Turn_Red = 1;
+        Turn_Green = 0;
+        BUS_Green =0 ;
+        BUS_Red = 1;
+        wait(3);
+        break;
+
+    }
+    case W_E_STATE_Wait:{
+      Emergency_pushed = true;
+      break;
+    }
   }
 }
 
 void state_machine() {
-    
-  curr_state = transition[(curr_state+1) % 9];
-  
+  curr_state = (curr_state+ 1) % 4;
   switch (curr_state){
-    case N_S_STATE_Wait:
-    case N_S_STATE_Go:
-    case W_E_STATE_Wait:
-    case W_E_STATE_Go:
-    case Emergency_STATE:
-    case TURN_STATE_Go:
-    case TURN_STATE_Wait:
-    case BUS_STATE_Go:
-    case BUS_STATE_Wait:
-
+     case N_S_STATE_Go :{
+      N_S_STATE_Go_task();
+      break;
+    }
+    case N_S_STATE_Wait:{
+      N_S_STATE_Wait_task();
+      break;
+    }
+     case W_E_STATE_Go:{
+      W_E_STATE_Go_task();
+      break;
+    }
+    case W_E_STATE_Wait:{
+      W_E_STATE_Wait_task();
+      break;
+    }
+   
+    // case TURN_STATE_Go:{
+    //   TURN_STATE_Go_task();
+    //   break;
+    // }
+    // case TURN_STATE_Wait:{
+    //   TURN_STATE_Wait_task();
+    //   break;
+    // }
+    // case BUS_STATE_Go:{
+    //   BUS_STATE_Go_task();
+    //   break;
+    // }
+    // case BUS_STATE_Wait:{
+    //   BUS_STATE_Wait_task();
+    //   break;
+    // }
   }
 
 }
